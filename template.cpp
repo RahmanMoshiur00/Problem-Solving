eac37fde3b6dc70919577176d02e6b1766d4e6a4
Md. Moshiur Rahman Munna <rahmanmoshiur623@gmail.com>
2024-03-04 22:08:24 +0600

    Add template code

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define debug           if( 0 )
+
+const int mxn = 100010;
+
+
+void solve(int tc)
+{
+
+}
+
+int main()
+{
+    //solve(1);
+    //int tc; cin>>tc; for(int t=1; t<=tc; t++) solve(t);
+
+    return 0;
+}
+
+
